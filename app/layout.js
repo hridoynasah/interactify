7db4c5c2339e6d3617829395c7dbbf8d9546b8d9
taseenjeem <taseenjeem.dev@gmail.com>
2024-12-05 23:52:11 +0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import localFont from "next/font/local";
+import { Barlow } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const fontStyle = Barlow({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
@@ -20,11 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={fontStyle.className}>{children}</body>
     </html>
   );
 }
