5a5101659f81284bb2ea228aabe745f0d7814c1b
taseenjeem <taseenjeem.dev@gmail.com>
2024-12-06 22:44:45 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import Hero from "@/components/page/landing-page/Hero";
 
 export default function LandingPage() {
-  return <>
-  <Hero></Hero>
-  </>;
+  return (
+    <>
+      <Hero></Hero>
+    </>
+  );
 }
