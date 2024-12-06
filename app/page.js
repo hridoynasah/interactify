e9d700754ae4fb261f31dc6550c5a50b3d6e1339
Sattik Siraj <sirajsattik@gmail.com>
2024-12-06 22:40:48 +0600

    added hero

--- app/page.js
+++ app/page.js
@@ -1,30 +1,7 @@
+import Hero from "@/components/page/landing-page/Hero";
+
 export default function LandingPage() {
-  return (
-    <>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
-        recusandae facere dolor iusto corporis minima, incidunt ea rem, ipsam
-        tempore et amet eum quos architecto! Culpa molestias, deserunt beatae
-        accusamus deleniti praesentium corrupti. Obcaecati animi hic iure
-        repellendus nostrum corrupti a, aut accusantium iusto et quia non
-        dolore, earum fugiat sint est consequatur. Voluptate amet assumenda quae
-        suscipit ab esse corrupti quis velit, magni quasi quas officiis, iusto
-        facere deserunt repudiandae consequuntur voluptatem fugiat accusamus
-        quidem iure. Natus, similique ullam officia quidem incidunt, minus culpa
-        laudantium eveniet voluptate quam rem explicabo at architecto
-        repudiandae nostrum? Voluptas pariatur omnis delectus doloremque, ea
-        iusto fugit sed ex molestias rem? Necessitatibus maiores dignissimos
-        fuga beatae blanditiis quis, consequuntur exercitationem eligendi ut,
-        illo fugit? Deserunt error ipsa quod voluptates debitis harum quibusdam
-        aut labore voluptatum, vel, quasi, et in! Assumenda enim necessitatibus
-        ex dolore fuga facilis vero consequuntur libero quas aperiam similique
-        vel non tenetur quisquam, eligendi ullam officia. Voluptatum quam ab
-        magni odit quas voluptatem cum, hic perferendis nemo aut earum quos
-        autem voluptatibus dignissimos ex aperiam provident velit! Repellat
-        dolorem aperiam iste optio assumenda blanditiis aut sunt accusantium
-        illo quasi recusandae, laboriosam fugiat voluptates nam magni dolor.
-        Molestias animi obcaecati repudiandae odio?
-      </p>
-    </>
-  );
+  return <>
+  <Hero></Hero>
+  </>;
 }
