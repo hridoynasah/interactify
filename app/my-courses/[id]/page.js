3c93b636f2b74fe3aee133290b49ba53d4842f92
taseenjeem <taseenjeem.dev@gmail.com>
2024-12-14 03:53:43 +0600

    Update page.js

--- app/my-courses/[id]/page.js
+++ app/my-courses/[id]/page.js
@@ -2,18 +2,23 @@ import CourseCard from "@/app/components/CourseCard";
 import { getUserPurchaseCourses } from "@/server-actions";
 
 export default async function MyCoursesPage({ params }) {
-  const { id } = await params;
+  const { id } = params;
   const courses = await getUserPurchaseCourses(id);
-  console.log("🚀 ~ MyCoursesPage ~ courses:", courses);
 
   return (
     <section className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-28">
       <h1 className="text-3xl font-bold mb-5 text-white">My Courses</h1>
-      <div className="grid md:grid-cols-3 gap-5">
-        {courses?.map((course) => (
-          <CourseCard key={course._id} course={course.course} purchased />
-        ))}
-      </div>
+      {courses && courses.length > 0 ? (
+        <div className="grid md:grid-cols-3 gap-5">
+          {courses.map((course) => (
+            <CourseCard key={course._id} course={course.course} purchased />
+          ))}
+        </div>
+      ) : (
+        <p className="text-xl text-gray-400">
+          You haven&apos;t purchased any courses yet.
+        </p>
+      )}
     </section>
   );
 }
